0fd3b2dfb47daf08e3aaeadca6f5b88b91186e58
Akash312003 <akashwiz31@gmail.com>
2024-02-28 22:19:39 +0530

    Photoland project

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import Logo from '../img/logo.png';
+import {SlBag} from 'react-icons/sl';
+import {FiMenu} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
+import SearchForm from '../components/SearchForm'
+import CategoryNavMoblie from '../components/CategoryNavMobile'
+import Cart from '../components/Cart'
+import { CartContext } from '../context/CartContext';
 
 const Header = () => {
-  return <div>Header</div>;
+  const {isOpen , setIsopen}=useContext(CartContext);
+  return <header>
+    <div className='container mx-auto'>
+      <div>
+        <div>
+          <FiMenu />
+        </div>
+        <div>
+          <CategoryNavMoblie />
+        </div>
+        <Link to={'/'}>
+        <img src={Logo} alt='' />
+        
+        </Link>
+        <div className='hidden w-full xl:flex xl:max-w-[734px]'> 
+          <SearchForm />
+        </div>
+        <div>
+          <div>Need Help? 1800 234 235 </div>
+          <div className='relative cursor-pointer'>
+            <SlBag className='text-2xl' />
+            <div>2</div>
+          </div>
+          <div className='bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300'>
+            <Cart />
+          </div>
+        </div>
+      </div>
+      <div className='lg:hidden'>
+        <SearchForm />
+      </div>
+    </div>
+  </header>
 };
 
 export default Header;
